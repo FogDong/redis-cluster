245e10a28d7777dafa3b10b3d99098560884ce85
Tianxin Dong <dongtianxin@caicloud.io>
2020-08-25 10:18:39 +0800

    Add redis cluster

--- start.sh
+++ start.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-h=`hostname`
 # Copy appropriate conf.d files from config-map to emptyDir.
 # ordinal 0 is the first host
 mkdir -p /usr/local/etc/redis
-if [ ! -f "/usr/local/etc/redis/redis.conf" ]; then
+if [ ! -f "$myFile" ]; then
    touch /usr/local/etc/redis/redis.conf
 fi
 
-result=$(echo $h | grep "\-0$")
+result=$(ip -4 address|grep "${VM_MASTER}/")
 if [ -n "$result" ]; then
   echo "yes"
 else
-  echo "slaveof redis-cluster-0.redis-cluster 6379" >> /usr/local/etc/redis/redis.conf
+  echo "slaveof  &VM_MASTER 6379" >> /usr/local/etc/redis/redis.conf
 fi
 
 redis-server /usr/local/etc/redis/redis.conf
